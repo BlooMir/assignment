419bcf9952a92713759503b8b0c9a7d9dce9e3a3
LSY <112356856+OWL-AT-Midnight@users.noreply.github.com>
2023-01-03 23:43:58 +0900

    chapter8 완료

---
+++ js/chapter8/Ch8_Quiz2.js
@@ -0,0 +1,23 @@
+const seed = document.querySelector("#seed");
+const prize = document.querySelector("#total");
+const result = document.querySelector("#result");
+const btn = document.querySelector("#raffle");
+
+let prizePeople = [];
+
+btn.addEventListener("click", (e) => {
+    for(i=0; i < prize.value; i++){
+        prizePeople.push(Math.floor( Math.random() * seed.value )); 
+    }
+
+    result.classList.add("show");
+    result.innerText = "당첨자 : ";
+
+    prizePeople.forEach(function(value){
+        result.innerText += " " + value + "번";
+    });
+    console.log(prizePeople);
+    e.preventDefault();
+});
+
+

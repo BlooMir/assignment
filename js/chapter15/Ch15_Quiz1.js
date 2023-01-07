d2796b6076a234e087fc4ad4dee98c6de11cdb88
LSY <112356856+OWL-AT-Midnight@users.noreply.github.com>
2023-01-07 20:13:54 +0900

    chapter14,15,16 complete

---
+++ js/chapter15/Ch15_Quiz1.js
@@ -0,0 +1,6 @@
+const canvas = document.querySelector("canvas");
+const ctx = canvas.getContext("2d");
+
+ctx.beginPath();
+ctx.font = "bold italic 300px sans-serif";
+ctx.fillText("DAMN", 50, 320);
